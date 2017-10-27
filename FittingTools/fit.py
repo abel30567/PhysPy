b8056343c6c58249eacccbae42c15fe2291470a2
JustinCata <jcata018@fiu.edu>
2017-10-27 02:45:52 -0400

    Counting Stats ^date

---
+++ FittingTools/fit.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 26 19:58:48 2017
+
+@author: JustinCata
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.mlab  as mlab
+from uncertainties import unumpy
+
